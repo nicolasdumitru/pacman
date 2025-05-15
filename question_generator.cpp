748d045a7e60bd2494dd4b57b796e56cedaf65aa
nicolasdumitru <nicolas@ndumitru.com>
2025-05-15 10:24:25 +0300

    WIP 6

---
+++ question_generator.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    std::cout <<  "{\n"
+      "\"Question\": \"The question text here\",\n"
+      "\"AnswerA\": \"Option A text\",\n"
+      "\"AnswerB\": \"Option B text\",\n"
+      "\"AnswerC\": \"Option C text\",\n"
+      "\"AnswerD\": \"Option D text\",\n"
+      "\"CorrectAnswer\": \"B\"\n"
+      "}\n";
+    return 0;
+}
